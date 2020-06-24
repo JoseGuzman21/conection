b476dc7c4546650edae4ad53c1e502ba734250d2
JoseGuzman21 <joseguzman@upeu.edu.pe>
2020-06-24 02:37:45 +0000

    firstt

---
+++ copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#tail -n+1 /home/joseguzman/central/reportest/reportram.txt >> /home/joseguzman/reporte/bd/ram.txt
+tail -n+1 /home/joseguzman/central/bd/red.txt >> /home/joseguzman/reporte/bd/red.txt
+tail -n+1 /home/joseguzman/central/bd/otros.txt >> /home/joseguzman/reporte/bd/otros.txt
